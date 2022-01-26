92a157ad46fc8cef1754880170b386534c0f4681
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-26 15:22:30 +0100

    translated quiz

--- scripts/translate.js
+++ scripts/translate.js
@@ -3,10 +3,16 @@ import translationPl from "../translations/pl.js";
 
 const toTranslate = document.querySelectorAll(".translate");
 
+const hyperlinks = document.querySelectorAll(".hyperlink");
+
 const lang = findLanguage();
 
 toTranslate.forEach((element) => {
   element.textContent = translateText(element.textContent, lang);
+});
+
+hyperlinks.forEach((element) => {
+  element.href = appendLanguage(element.href, lang);
 });
 
 function findLanguage() {
@@ -33,5 +39,18 @@ function translateText(content, lang) {
 
     default:
       return content;
+  }
+}
+
+function appendLanguage(href, lang) {
+  switch (lang) {
+    case "de":
+      return href += "?lang=de";
+
+    case "pl":
+      return href += "?lang=pl";
+
+    default:
+      return href += "?lang=en";
   }
 }
