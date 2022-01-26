34ff2a699601e85d4314d099131918ec70021f63
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-26 14:41:37 +0100

    tiny changes

--- translations/de.js
+++ translations/de.js
@@ -1,6 +1,7 @@
 const translations = {
   "Text to translate": "Text zum übersetzen",
   "Answer": "Antwort",
+  "Question": "Pytanie",
   "What kind of pizza are you?": "Welche Pizza bist du?",
   "Choose a quiz": "Wähle einen Quiz",
 };
--- translations/pl.js
+++ translations/pl.js
@@ -1,6 +1,7 @@
 const translations = {
   "Text to translate": "Tekst do tłumaczenia",
   "Answer": "odpowiedź",
+  "Question": "Pytanie",
   "What kind of pizza are you?": "Jakim rodzajem pizzy jesteś?",
   "Choose a quiz": "Wybierz quiz",
 };
