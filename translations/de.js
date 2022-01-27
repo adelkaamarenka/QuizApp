d01cd3665b87d35b7d589d15fac01cc118fad18b
adelkaamarenka <adelka.dilaj@gmail.com>
2022-01-27 16:51:37 +0100

    german translations

--- translations/de.js
+++ translations/de.js
@@ -1,7 +1,10 @@
 const translations = {
   "Text to translate": "Text zum übersetzen",
-  "Answer": "Antwort",
-  "Question": "Pytanie",
+  "answer": "Antwort",
+  "Question": "Frage",
+  "Question 1": "Frage 1",
+  "Question 2": "Frage 2",
+  "Question 3": "Frage 3",
   "What kind of pizza are you?": "Welche Pizza bist du?",
   "Choose a quiz": "Wähle einen Quiz",
 };
