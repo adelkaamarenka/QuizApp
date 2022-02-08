a88feaaa8f0ce112ea4f61da0ff5149e52ffb22f
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-09 00:29:17 +0100

    added the content displaying

--- scripts/result.js
+++ scripts/result.js
@@ -1,3 +1,33 @@
 import { getFinalAnswer } from "./load-quiz.js";
+import { translateText } from "./translate.js";
+import langExport from "./translate.js";
+import { resultContent } from "./result-content.js";
 
-console.log(getFinalAnswer());
+let elementsArr = ["title", "img", "description"];
+
+let resultElements = {
+    title: "",
+    img: "",
+    description: "",
+}
+
+let lang = langExport;
+
+let answer = getFinalAnswer();
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    elementsArr.forEach(element => {
+        resultElements[element] = document.querySelector(`.${element}`);
+        console.log(resultElements[element]);
+    })
+
+    loadContent();
+}
+
+function loadContent() {
+    elementsArr.forEach(element => {
+        resultElements[element].textContent = translateText(resultContent[answer][element], lang);
+    })
+}
