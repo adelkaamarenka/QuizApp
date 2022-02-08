9c4ddf3156b9a45a6dc051413c4a53c49e59b94e
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-08 23:18:52 +0100

    added result content file

---
+++ scripts/result-content.js
@@ -0,0 +1,22 @@
+const resultContent = {
+    a: {
+        title: "Result 1",
+        img: "",
+        description: "description",
+    },
+    b: {
+        title: "Result 2",
+        img: "",
+        description: "description",
+    },
+    c: {
+        title: "Result 3",
+        img: "",
+        description: "description",
+    },
+    d: {
+        title: "Result 4",
+        img: "",
+        description: "description",
+    },
+}
