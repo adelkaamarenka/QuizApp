b0a76c264129d1812a0985ece9500abe64fa4b37
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-08 19:40:25 +0100

    updated the translations

--- translations/de.js
+++ translations/de.js
@@ -5,6 +5,8 @@ const translations = {
   "Question 1": "Frage 1",
   "Question 2": "Frage 2",
   "Question 3": "Frage 3",
+  "Question 4": "Frage 4",
+  "Question 5": "Frage 5",
   "What kind of pizza are you?": "Welche Pizza bist du?",
   "Choose a quiz": "Wähle einen Quiz",
 };
