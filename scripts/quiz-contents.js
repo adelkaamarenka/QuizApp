9e24d8edd9ced65128a70be243f9151e62598d9e
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-09 22:56:29 +0100

    added the actual quiz content

--- scripts/quiz-contents.js
+++ scripts/quiz-contents.js
@@ -1,39 +1,40 @@
 const quizContent = [
     {
-        question: "Question 1",
-        a: "answer",
-        b: "answer",
-        c: "answer",
-        d: "answer",
+        //a -> Pinapple  b-> Pepperoni  c -> Margherita   d-> Olives
+        question: "What does your perfect holiday look like?",
+        a: "Spontaneous trip to an unknown destination",
+        b: "All-inclusive holiday in Spain with my friends",
+        c: "Relaxing somewhere by a nearby lake with a close friend or partner",
+        d: "Visiting an European capital",
     },
     {
-        question: "Question 2",
-        a: "answer",
-        b: "answer",
-        c: "answer",
-        d: "answer",
-    },
-    /* {
-        question: "Question 3",
-        a: "answer",
-        b: "answer",
-        c: "answer",
-        d: "answer",
+        question: "What does your wardrobe mostly consist of?",
+        a: "Crazy patterns and silhouettes",
+        b: "A little bit of everything",
+        c: "Simple t-shirts, hoodies and jeans ",
+        d: "Minimalistic and expensive designer clothes ",
     },
     {
-        question: "Question 4",
-        a: "answer",
-        b: "answer",
-        c: "answer",
-        d: "answer",
+        question: "How would you like to spend your birthday?",
+        a: "A surprise party organized by my friends ",
+        b: "A huge party to which all the people I know would be invited ",
+        c: "A film marathon alone or with a close friend",
+        d: "Dinner in a fancy restaurant with my partner ",
     },
     {
-        question: "Question 5",
-        a: "answer",
-        b: "answer",
-        c: "answer",
-        d: "answer",
-    }, */
+        question: "At a party I usually:",
+        a: "Dance till I’m dead!",
+        b: "Engage in conversations in the kitchen",
+        c: "Hang out alone on the couch or cling to the friend I came with",
+        d: "Look good and flirt with people",
+    },
+    {
+        question: "What do you enjoy doing in your free time?",
+        a: "Doing some sports outdoors",
+        b: "Going out with friends",
+        c: "Reading my favorite book",
+        d: "Doing some sports outdoors",
+    },
 ];
 
 export default quizContent;
