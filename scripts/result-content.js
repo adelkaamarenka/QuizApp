286c2f9e9215046370a3173e1844af8150560872
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-09 22:57:51 +0100

    added the results

--- scripts/result-content.js
+++ scripts/result-content.js
@@ -1,22 +1,22 @@
 export const resultContent = {
     a: {
-        title: "Result 1",
+        title: "Pineapple pizza",
         img: "",
-        description: "description",
+        description: "Your decisions often leave others confused, but you don’t care. You’re extravagant. You often struggle with conflicting emotions, have lots of energy and crazy ideas, that’s why people either love of hate you.",
     },
     b: {
-        title: "Result 2",
+        title: "Pepperoni pizza",
         img: "",
-        description: "description",
+        description: "Everyone knows you and they all love you. You’re extroverted and the life of the party. People can never be bored around you!",
     },
     c: {
-        title: "Result 3",
+        title: "Margherita",
         img: "",
-        description: "description",
+        description: "You like simplicity and minimalism. You prefer quality over quality, so at home you only have one plant you take good care of, the same favourite bag you’ve had for years and the one best friend you’ve known since kindergarten. You enjoy your own company and have no problem traveling or eating at a restaurant alone.",
     },
     d: {
-        title: "Result 4",
+        title: "Olives and artichoke pizza",
         img: "",
-        description: "description",
+        description: "You're an intellectual that appreciates the finer things that life has to offer. You have sophisticated taste when it comes to fashion, friends, and food.",
     },
 }
