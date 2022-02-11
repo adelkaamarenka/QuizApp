e87fa9e6c3ee2d9a8455517bd44e2c3b2d05c8bb
adelkaamarenka <adelka.dilaj@gmail.com>
2022-02-11 22:51:05 +0100

    added img urls

--- scripts/result-content.js
+++ scripts/result-content.js
@@ -1,22 +1,22 @@
 export const resultContent = {
     a: {
         title: "Pineapple pizza",
-        img: "",
+        img: "./images/Pizzas/hawaii3.jpg",
         description: "Your decisions often leave others confused, but you don’t care. You’re extravagant. You often struggle with conflicting emotions, have lots of energy and crazy ideas, that’s why people either love of hate you.",
     },
     b: {
         title: "Pepperoni pizza",
-        img: "",
+        img: "./images/Pizzas/pepperoni.jpg",
         description: "Everyone knows you and they all love you. You’re extroverted and the life of the party. People can never be bored around you!",
     },
     c: {
         title: "Margherita",
-        img: "",
+        img: "./images/Pizzas/margherita.jpg",
         description: "You like simplicity and minimalism. You prefer quality over quality, so at home you only have one plant you take good care of, the same favourite bag you’ve had for years and the one best friend you’ve known since kindergarten. You enjoy your own company and have no problem traveling or eating at a restaurant alone.",
     },
     d: {
         title: "Olives and artichoke pizza",
-        img: "",
+        img: "./images/Pizzas/oliven.jpg",
         description: "You're an intellectual that appreciates the finer things that life has to offer. You have sophisticated taste when it comes to fashion, friends, and food.",
     },
 }
